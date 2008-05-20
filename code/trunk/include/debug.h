dfeb4c13997606c27c92b00bdf7ee9e1dd81d693
Harald Kraemer <hkr@snet-home.org>
2008-05-20 19:06:28 +0000

    debug methods

---
+++ code/trunk/include/debug.h
@@ -0,0 +1,20 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "stdarg.h"
+#include "assert.h"
+
+/*
+ * reports an error and terminates the application after that.
+ * parameters are equal to printf.
+ */
+extern void SNetUtilDebugFatal(char* message, ...);
+
+/*
+ * reports an error.
+ * parameters are equal to printf.
+ */
+extern void SNetUtilDebugNotice(char* message, ...);
+#endif
---
+++ code/trunk/src/runtime/utility/debug.c
@@ -0,0 +1,18 @@
+#include "debug.h"
+
+extern void SNetUtilDebugFatal(char* m, ...) {
+    va_list p;
+
+    va_start(p, m);
+    fprintf(stderr, "\n\n ** FATAL ERROR ** ");
+    vfprintf(stderr, m, p);
+    exit(-1);
+}
+
+extern void SNetUtilDebugNotice(char *m, ...) {
+    va_list p;
+
+    va_start(p, m);
+    fprintf(stderr, "\n\n ** RUNTIME NOTICE ** ");
+    vfprintf(stderr, m, p);
+}
