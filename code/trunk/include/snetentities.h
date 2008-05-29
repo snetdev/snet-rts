4f2a2bd68333f39c05d7f2c684a1a5594c5ca25c
Harald Kraemer <hkr@snet-home.org>
2008-05-29 12:31:46 +0000

    further factoring work

--- code/trunk/include/snetentities.h
+++ code/trunk/include/snetentities.h
@@ -14,147 +14,15 @@
 #include <expression.h>
 #include <stdio.h>
 
-#define SNET_STAR_COUNTERFIELD "star_counter"
+#include "filter.h"
+#include "box.h"
+#include "collectors.h"
+#include "interface_functions.h"
+#include "out.h"
+#include "parallel.h"
+#include "serial.h"
+#include "split.h"
+#include "star.h"
+#include "syncro.h"
 
-
-#ifdef DBG_RT_TRACE_THREAD_CREATE
-  extern int SNetGetThreadCount();
-  extern int SNetIncThreadCount();
-  extern void SNetLockThreadMutex();
-  extern void SNetUnlockThreadMutex();
-#endif
-
-
-extern bool SNetGlobalInitialise();
-extern bool 
-SNetGlobalRegisterInterface( int id, 
-                             void (*freefun)( void*),
-                             void* (*copyfun)( void*),
-			     int (*serfun)( FILE *, void*),
-                             void* (*deserfun)(FILE *));
-
-
-extern snet_filter_instruction_t *SNetCreateFilterInstruction( snet_filter_opcode_t opcode, ...);
-extern void SNetDestroyFilterInstruction( snet_filter_instruction_t *instr);
-extern snet_filter_instruction_set_t *SNetCreateFilterInstructionSet( int num, ...);	
-extern void SNetDestroyFilterInstructionSet( snet_filter_instruction_set_t *set); 
-extern snet_filter_instruction_set_list_t *SNetCreateFilterInstructionSetList( int num, ...);
-extern int SNetFilterGetNumInstructions( snet_filter_instruction_set_t *set);
-
-extern snet_handle_t *SNetOutRaw( snet_handle_t *hnd, int variant_num, ...);
-
-extern snet_handle_t *SNetOut( snet_handle_t *hnd, snet_record_t *rec);
-
-extern snet_handle_t 
-*SNetOutRawArray( snet_handle_t *hnd, 
-		  int if_id,
-                  int var_num,
-                  void **fields,
-                  int *tags,
-                  int *btags);
-
-
-extern snet_buffer_t *SNetBox( snet_buffer_t *inbuf, 
-			       void (*boxfun)( snet_handle_t*), 
-			       snet_typeencoding_t *outspec);
-
-	
-extern snet_buffer_t *SNetAlias( snet_buffer_t *inbuf,
-                                 snet_buffer_t *(*net)(snet_buffer_t*));
-
-extern snet_buffer_t *SNetSerial( snet_buffer_t *inbuf, 
-				  snet_buffer_t* (*box_a)( snet_buffer_t*), 
-                                  snet_buffer_t* (*box_b)( snet_buffer_t*)); 
-
-extern snet_buffer_t *SNetParallel( snet_buffer_t *inbuf,
-				    snet_typeencoding_list_t *types, 
-				    ...);
-
-
-
-extern snet_buffer_t *SNetParallelDet( snet_buffer_t *inbuf, 
-				       snet_typeencoding_list_t *types,
-				       ...);
-
-
-//extern void SNETstarDispatcher( snet_buffer_t *from, snet_buffer_t *to);
-extern snet_buffer_t *SNetStar( snet_buffer_t *inbuf, 
-				snet_typeencoding_t *type,
-				snet_expr_list_t *guards,
-				snet_buffer_t* (*box_a)(snet_buffer_t*),  
-                           	snet_buffer_t* (*box_b)(snet_buffer_t*));
-
-
-
-extern snet_buffer_t *SNetStarIncarnate( snet_buffer_t *inbuf, 
-					 snet_typeencoding_t *type,
-					 snet_expr_list_t *guards,
-					 snet_buffer_t* (*box_a)(snet_buffer_t*),  
-                           		 snet_buffer_t* (*box_b)(snet_buffer_t*));
-
-extern snet_buffer_t *SNetStarDet( snet_buffer_t *inbuf,
-				   snet_typeencoding_t *type,
-				   snet_expr_list_t *guards,
-				   snet_buffer_t* (*box_a)(snet_buffer_t*),  
-                           	   snet_buffer_t* (*box_b)(snet_buffer_t*));
-
-extern snet_buffer_t *SNetStarDetIncarnate( snet_buffer_t *inbuf, 
-					    snet_typeencoding_t *type,
-					    snet_expr_list_t *guards,
-					    snet_buffer_t* (*box_a)(snet_buffer_t*),  
-                           		    snet_buffer_t* (*box_b)(snet_buffer_t*));
-
-
-extern snet_buffer_t *SNetSync( snet_buffer_t *inbuf, 
-		  	        snet_typeencoding_t *outtype, 
-				snet_typeencoding_t *patterns,
-				snet_expr_list_t *guards);
-
-
-extern snet_buffer_t *SNetSplit( snet_buffer_t *inbuf, 
-				 snet_buffer_t* (*box_a)( snet_buffer_t*),
-                            	 int ltag, 
-				 int utag);
-
-extern snet_buffer_t *SNetSplitDet( snet_buffer_t *inbuf, 
-				    snet_buffer_t* (*box_a)( snet_buffer_t*),
-                            	    int ltag, int utag);
-
-#ifdef FILTER_VERSION_2
-extern snet_buffer_t *SNetFilter( snet_buffer_t *inbuf,
-		                  snet_typeencoding_t *in_type,
-		                  snet_expr_list_t *guards, ... );
-
-extern snet_buffer_t *SNetTranslate( snet_buffer_t *inbuf,
-		                     snet_typeencoding_t *in_type,
-		                     snet_expr_list_t *guards, ... );
-
-extern snet_buffer_t 
-*SNetNameShift( snet_buffer_t *inbuf, 
-                int offset,
-                snet_variantencoding_t *untouched);
-#else
-
-extern snet_buffer_t *SNetFilter( snet_buffer_t *inbuf,
-				  snet_typeencoding_t *in_type,
-				  snet_typeencoding_list_t *out_types,
-				  snet_expr_list_t *guards,  ...);
-
-//extern snet_buffer_t *SNetFilter( snet_buffer_t *inbuf, 
-//				  snet_typeencoding_t *in_type,
-//				  snet_typeencoding_t *out_type, 
-//				  snet_filter_instruction_set_list_t *instr,
-//				  snet_expr_list_t *guards);
-extern snet_buffer_t *SNetTranslate( snet_buffer_t *inbuf, 
-				     snet_typeencoding_t *in_type,
-				     snet_typeencoding_t *out_type, ...);
-#endif
-extern void *SNetGetCopyFun( int id);
-extern void *SNetGetCopyFunFromRec( snet_record_t *rec);
-extern void *SNetGetFreeFun( int id);
-extern void *SNetGetFreeFunFromRec( snet_record_t *rec);
-extern void *SNetGetSerializationFun( int id);
-extern void *SNetGetSerializationFunFromRec( snet_record_t *rec);
-extern void *SNetGetDeserializationFun( int id);
-extern void *SNetGetDeserializationFunFromRec( snet_record_t *rec);
 #endif
--- code/trunk/include/syncro.h
+++ code/trunk/include/syncro.h
@@ -4,21 +4,6 @@
 #include "typeencode.h"
 #include "buffer.h"
 
-typedef struct {
-  bool is_match;
-  int match_count;
-} match_count_t;
-
-typedef struct {
-  snet_buffer_t *from_buf;
-  snet_buffer_t *to_buf;
-  char *desc;
-} dispatch_info_t;
-
-typedef struct {
-  snet_buffer_t *buf;
-  int num;
-} snet_blist_elem_t;
 
 snet_buffer_t *SNetSync( snet_buffer_t *input, 
                          snet_typeencoding_t *outtype,
