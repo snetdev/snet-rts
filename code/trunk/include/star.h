491c56ad405af4c595d0ca6b25e71f6aebbb5bb4
Jukka Julku <jukka.julku@vtt.fi>
2008-06-06 07:26:30 +0000

    Fixed inconsistency between star.c and star.h.

--- code/trunk/include/star.h
+++ code/trunk/include/star.h
@@ -23,8 +23,8 @@ snet_buffer_t *SNetStarIncarnate( snet_buffer_t *inbuf,
 extern snet_buffer_t *SNetStarDet( snet_buffer_t *inbuf,
                                    snet_typeencoding_t *type,
                                    snet_expr_list_t *guards,
-                                   snet_buffer_t* (*box_a)(snet_buffer_t*),
-                                   snet_buffer_t (*box_b)(snet_buffer_t*));
+                                   snet_buffer_t *(*box_a)(snet_buffer_t*),
+                                   snet_buffer_t *(*box_b)(snet_buffer_t*));
 extern
 snet_buffer_t *SNetStarDetIncarnate( snet_buffer_t *inbuf,
                                snet_typeencoding_t *type,
