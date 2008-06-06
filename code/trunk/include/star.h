d740e61761deb5b6207c6ce546df8c5e6021db71
Harald Kraemer <hkr@snet-home.org>
2008-06-06 06:10:33 +0000

    during refactoring, I forgot the SNetStarDet. Added it again now.

--- code/trunk/include/star.h
+++ code/trunk/include/star.h
@@ -5,7 +5,7 @@
 #include "typeencode.h"
 #include "expression.h"
 
-extern snet_buffer_t *SNetStar( snet_buffer_t *inbuf, 
+extern snet_buffer_t *SNetStar( snet_buffer_t *inbuf,
                                 snet_typeencoding_t *type,
                                 snet_expr_list_t *guards,
                                 snet_buffer_t* (*box_a)(snet_buffer_t*),
@@ -20,16 +20,14 @@ snet_buffer_t *SNetStarIncarnate( snet_buffer_t *inbuf,
                                   snet_buffer_t *(*box_b)(snet_buffer_t*));
 
 
-extern
-snet_buffer_t *SNetStarIncarnate( snet_buffer_t *inbuf,
-                            snet_typeencoding_t *type,
-                               snet_expr_list_t *guards,
-                                  snet_buffer_t *(*box_a)(snet_buffer_t*),
-                                  snet_buffer_t *(*box_b)(snet_buffer_t*));
-
+extern snet_buffer_t *SNetStarDet( snet_buffer_t *inbuf,
+                                   snet_typeencoding_t *type,
+                                   snet_expr_list_t *guards,
+                                   snet_buffer_t* (*box_a)(snet_buffer_t*),
+                                   snet_buffer_t (*box_b)(snet_buffer_t*));
 extern
 snet_buffer_t *SNetStarDetIncarnate( snet_buffer_t *inbuf,
-                               snet_typeencoding_t *type,  
+                               snet_typeencoding_t *type,
                                   snet_expr_list_t *guards,
                                      snet_buffer_t *(*box_a)(snet_buffer_t*),
                                      snet_buffer_t *(*box_b)(snet_buffer_t*));
