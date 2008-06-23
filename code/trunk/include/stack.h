778995e64c8c6426f7e233958770ce556beedbd9
Harald Kraemer <hkr@snet-home.org>
2008-06-23 20:37:36 +0000

    forgot to include this in the previous commit, look there

--- code/trunk/include/stack.h
+++ code/trunk/include/stack.h
@@ -4,21 +4,23 @@
 #include "bool.h"
 
 typedef struct stack snet_util_stack_t;
+typedef struct stack_iterator snet_util_stack_iterator_t;
 
 snet_util_stack_t *SNetUtilStackCreate();
 void SNetUtilStackDestroy(snet_util_stack_t *target);
 
 bool SNetUtilStackIsEmpty(snet_util_stack_t *target);
-void SNetUtilStackPush(snet_util_stack_t *target, void *content);
-void SNetUtilStackPop(snet_util_stack_t *target);
-void *SNetUtilStackPeek(snet_util_stack_t *target);
+snet_util_stack_t *SNetUtilStackPush(snet_util_stack_t *target, int content);
+snet_util_stack_t *SNetUtilStackPop(snet_util_stack_t *target);
+int SNetUtilStackPeek(snet_util_stack_t *target);
+snet_util_stack_t *SNetUtilStackSet(snet_util_stack_t *target, int new_value);
 
 
-void SNetUtilStackGotoTop(snet_util_stack_t *target);
-void SNetUtilStackGotoBottom(snet_util_stack_t *target); 
-void SNetUtilStackUp(snet_util_stack_t *target);
-void SNetUtilStackDown(snet_util_stack_t *target);
-void *SNetUtilStackGet(snet_util_stack_t *target);
-bool SNetUtilStackCurrentDefined(snet_util_stack_t *target);
-
+snet_util_stack_iterator_t *SNetUtilStackTop(snet_util_stack_t *target);
+snet_util_stack_iterator_t *SNetUtilStackBottom(snet_util_stack_t *target);
+void SNetUtilStackIterDestroy(snet_util_stack_iterator_t *target);
+snet_util_stack_iterator_t *
+SNetUtilStackIterNext(snet_util_stack_iterator_t *target);
+bool SNetUtilStackIterCurrDefined(snet_util_stack_iterator_t *target);
+int SNetUtilStackIterGet(snet_util_stack_iterator_t *target);
 #endif
