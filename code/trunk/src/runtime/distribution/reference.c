d1b9401da7b2df721c169a39d9b66c0ff4fb5c1d
Jukka Julku <jukka.julku@vtt.fi>
2009-01-13 07:55:57 +0000

    - Improved locking for references:   - A reference is not locked during remote fetch anymore, but other threads may use the reference as normal.     - Another get/take-call on data during a fetch causes the calling thread to block until the fetch completes. - Added Doxygen comments for each function on reference.c

--- code/trunk/src/runtime/distribution/reference.c
+++ code/trunk/src/runtime/distribution/reference.c
@@ -1,5 +1,39 @@
 #ifdef DISTRIBUTED_SNET
 
+/** <!--********************************************************************-->
+ * $Id$
+ *
+ * @file reference.c
+ *
+ * @brief Implements reference system used in distributed S-Net.
+ *
+ * The reference system counts references to a data. 
+ * 
+ * Each node always contains a single reference for each (local or remote) data element that can be 
+ * referred from the node and for each data element located in the node that can be remotely 
+ * referred from other nodes.
+ *
+ * References are stored in data storage (@ref datastorage.h)
+ *
+ * The main functions of references is to hide remote data items by creating a local version of the 
+ * reference and to make it possible to avoid remote data operations.
+ *
+ *
+ * @author Jukka Julku, VTT Technical Research Centre of Finland
+ *
+ * @date 13.1.2009
+ *
+ *****************************************************************************/
+
+/* NOTICE:
+ *
+ * To avoid deadlocks between reference's and datastorage's mutexes, 
+ * a thread holding mutex to a reference must release the mutex before 
+ * calling any of the data storage's functions execept remote fetch, copy
+ * and delte functions, which do not lock the data storage.
+ *
+ */
+
 #include <pthread.h>
 
 #include "reference.h"
@@ -7,14 +41,121 @@
 #include "interface_functions.h"
 #include "datastorage.h"
 
+/** <!--********************************************************************-->
+ *
+ * @name Ref
+ *
+ * <!--
+ * snet_ref_t *SNetRefCreate(snet_id_t id, int location, int interface, void *data) : Creates a new reference
+ * snet_ref_t *SNetRefDestroy(snet_ref_t *ref) : Deletes the reference
+ * snet_ref_t *SNetRefCopy(snet_ref_t *ref) : Copies the reference
+ * int SNetRefGetRefCount(snet_ref_t *ref) : Returns the reference count
+ * void *SNetRefGetData(snet_ref_t *ref) : Returns the data behind the reference
+ * void *SNetRefTakeData(snet_ref_t *ref) : Returns the data behind the reference
+ * int SNetRefGetInterface(snet_ref_t *ref) : Returns language interface
+ * int SNetRefPack(snet_ref_t *ref, MPI_Comm comm, int *pos, void *buf, int buf_size) : Packs reference into buffer
+ * snet_ref_t *SNetRefUnpack(MPI_Comm comm, int *pos, void *buf, int buf_size) : Unpacks reference from buffer
+ * -->
+ *
+ *****************************************************************************/
+/*@{*/
+
+/** @struct snet_ref
+ *
+ *   @brief Reference type.
+ *
+ */
+
 struct snet_ref {
-  snet_id_t id;
-  unsigned int location;
-  int interface;
-  unsigned int ref_count;
-  void *data;
-  pthread_mutex_t mutex; 
+  snet_id_t id;           /**< Id of the data. */
+  unsigned int location;  /**< Location of the data which is referred. */
+  int interface;          /**< Language interface of the data. */
+  unsigned int ref_count; /**< Count of references to the data.  */
+  void *data;             /**< The actual data, or NULL if the data is not local. */
+  pthread_mutex_t mutex;  /**< Mutex guarding access to the reference. */
+  bool is_fetching;       /**< Information on if some thread is currently doing remote fetch. */
+  pthread_cond_t ready;   /**< Condition used to signal ending of remote fetch. */
 };
+
+
+/** <!--********************************************************************-->
+ *
+ * @fn  static void SNetRefFetch(snet_ref_t *ref) 
+ *
+ *   @brief  Fetches the data from remote node, or waits until the data is
+ *           fetched in case another thread is currently fetching the data.
+ *
+ *   @note   The call starts a remote fetch operation.   
+ *
+ *   @param ref  The reference.
+ *
+ ******************************************************************************/
+
+static void SNetRefFetch(snet_ref_t *ref) 
+{
+  void *data = NULL;
+  snet_id_t id;
+  int interface;
+  int location;
+
+  if(!ref->is_fetching) {
+    
+    /* Fetch the data. */
+    
+    id = ref->id;
+    interface = ref->interface;
+    location = ref->location;
+    
+    ref->location = SNetIDServiceGetNodeID();
+    
+    ref->is_fetching = true;
+    
+    pthread_mutex_unlock(&ref->mutex);
+    
+    
+    data = SNetDataStorageRemoteFetch(id, interface, location);
+    
+    
+    pthread_mutex_lock(&ref->mutex);
+    
+    ref->data = data;
+    
+    ref->is_fetching = false;
+    
+    pthread_cond_signal(&ref->ready);
+  } else {
+    
+    /* Wait until the data is fetched. */
+    
+    while(ref->is_fetching) {
+      pthread_cond_wait(&ref->ready, &ref->mutex);
+    }
+  }
+
+  return;
+}
+
+
+/** <!--********************************************************************-->
+ *
+ * @fn  snet_ref_t *SNetRefCreate(snet_id_t id, int location, int interface, void *data)
+ *
+ *   @brief  Creates a new reference, or copies an old reference with the same id.
+ *
+ *           If a reference is created for a remote data element, but there already
+ *           is a reference with the same id, the reference is instead copied and
+ *           the remote reference is deleted.
+ *
+ *   @note   In case of non-local data, the call may start a remote delete operation. 
+ *
+ *   @param id         Unique identifier of the data.
+ *   @param location   Location of the data.
+ *   @param interface  Langauge interface of the data. 
+ *   @param data       The actual data, or NULL if the data is remote. 
+ *
+ *   @return           A new reference, or copy of an old reference.
+ *
+ ******************************************************************************/
 
 snet_ref_t *SNetRefCreate(snet_id_t id, int location, 
 			  int interface, void *data)
@@ -35,6 +176,10 @@ snet_ref_t *SNetRefCreate(snet_id_t id, int location,
     ref->interface = interface;
     ref->ref_count = 1;
 
+    ref->is_fetching = false;
+    
+    pthread_cond_init(&ref->ready, NULL);
+
     ref->data = data;
     
     pthread_mutex_init(&ref->mutex, NULL);
@@ -48,24 +193,53 @@ snet_ref_t *SNetRefCreate(snet_id_t id, int location,
   return ref;
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  snet_ref_t *SNetRefDestroy(snet_ref_t *ref) 
+ *
+ *   @brief  Destroys the given reference.
+ *
+ *           Because of the reference counting system, the reference count is 
+ *           actually decreased by one, and the reference is only destroyed if 
+ *           the reference count hits zero. In this case the reference is also 
+ *           removed from the data storage.    
+ *
+ *   @note   In case of non-local data, the call may start a remote delete operation.   
+ *
+ *   @param ref  The reference to be destroyed.
+ *
+ *   @return     Pointer to the reference, or NULL if the reference was freed.
+ *
+ ******************************************************************************/
+
+
 snet_ref_t *SNetRefDestroy(snet_ref_t *ref) 
 {
-  bool b;
+  bool is_removed;
 
   pthread_mutex_lock(&ref->mutex);
 
   if(ref->ref_count == 1) {
 
+    /* Try to remove the reference.
+     *
+     * It is possible that the reference is copied 
+     * before it is removed from the data storage.
+     * In this case the reference is not destroyed.
+     *
+     */
+
     do {
       pthread_mutex_unlock(&ref->mutex);
       
-      b = SNetDataStorageRemove(ref->id);
+      is_removed = SNetDataStorageRemove(ref->id);
     
       pthread_mutex_lock(&ref->mutex);
 
-     } while(!b && ref->ref_count == 1);
+     } while(!is_removed && ref->ref_count == 1);
       
-    if(b) {
+    if(is_removed) {
       if(ref->data != NULL) {
 	SNetGetFreeFun(ref->interface)(ref->data);
       } else {
@@ -95,6 +269,21 @@ snet_ref_t *SNetRefDestroy(snet_ref_t *ref)
   return ref;
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  snet_ref_t *SNetRefCopy(snet_ref_t *ref)
+ *
+ *   @brief  Copies the given reference.
+ *
+ *           Increases the reference count of the data by one.      
+ *
+ *   @param ref  The reference to be copied.
+ *
+ *   @return     Pointer to the reference.
+ *
+ ******************************************************************************/
+
 snet_ref_t *SNetRefCopy(snet_ref_t *ref)
 {
   pthread_mutex_lock(&ref->mutex);
@@ -105,6 +294,19 @@ snet_ref_t *SNetRefCopy(snet_ref_t *ref)
 
   return ref;
 }
+
+
+/** <!--********************************************************************-->
+ *
+ * @fn  int SNetRefGetRefCount(snet_ref_t *ref)
+ *
+ *   @brief  Returns the reference count of the data the reference is referring to. 
+ *
+ *   @param ref  The reference.
+ *
+ *   @return     Count of references.
+ *
+ ******************************************************************************/
 
 int SNetRefGetRefCount(snet_ref_t *ref)
 {
@@ -119,6 +321,27 @@ int SNetRefGetRefCount(snet_ref_t *ref)
   return ret;
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  void *SNetRefGetData(snet_ref_t *ref)
+ *
+ *   @brief  Returns the data element behind the reference.
+ *
+ *           The data is not copied and the reference stays as it is. 
+ *
+ *           This function corresponds to SNetRecGetField()
+ *
+ *   @note   If the data is consumed, use SNetRefTakeData() instead!
+ *
+ *   @note   In case of non-local data, the call may start a remote fetch operation.   
+ *
+ *   @param ref  The reference.
+ *
+ *   @return     A pointer to the data element.
+ *
+ ******************************************************************************/
+
 void *SNetRefGetData(snet_ref_t *ref)
 {
   void *data = NULL;
@@ -126,8 +349,9 @@ void *SNetRefGetData(snet_ref_t *ref)
   pthread_mutex_lock(&ref->mutex);
 
   if(ref->data == NULL) {
-    ref->data = SNetDataStorageRemoteFetch(ref->id, ref->interface, ref->location);
-    ref->location = SNetIDServiceGetNodeID();
+
+    SNetRefFetch(ref);
+ 
   }
 
   data = ref->data;
@@ -137,29 +361,60 @@ void *SNetRefGetData(snet_ref_t *ref)
   return data;
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  void *SNetRefTakeData(snet_ref_t *ref) 
+ *
+ *   @brief  Returns the data element behind the reference and destroys the reference.
+ *
+ *           If there are multiple references to the same data, the data is copied 
+ *           using the language interface functions and a copy is returned. The 
+ *           reference is destroyed as with SNetRefDestroy().  
+ *
+ *           This function corresponds to SNetRecTakeField()
+ *
+ *   @note   In case of non-local data, the call may start a remote fetch operation.   
+ *
+ *   @param ref  The reference.
+ *
+ *   @return     A pointer to the data element.
+ *
+ ******************************************************************************/
+
 void *SNetRefTakeData(snet_ref_t *ref) 
 {
-  void *data;
-  bool b;
+  void *data = NULL;
+  bool is_removed;
 
   pthread_mutex_lock(&ref->mutex);
 
   if(ref->data == NULL) {
-    ref->data = SNetDataStorageRemoteFetch(ref->id, ref->interface, ref->location);
-    ref->location = SNetIDServiceGetNodeID();
+
+    SNetRefFetch(ref);
+   
   }
 
   if(ref->ref_count == 1) {
+
+    /* Try to remove the reference.
+     *
+     * It is possible that the reference is copied 
+     * before it is removed from the data storage.
+     * In this case the reference is not destroyed.
+     *
+     */
+
     do {
       pthread_mutex_unlock(&ref->mutex);
       
-      b = SNetDataStorageRemove(ref->id);
+      is_removed = SNetDataStorageRemove(ref->id);
       
       pthread_mutex_lock(&ref->mutex);
 
-    } while(!b && ref->ref_count == 1);
+    } while(!is_removed && ref->ref_count == 1);
 
-    if(b) {
+    if(is_removed) {
 
       data = ref->data;
 
@@ -190,6 +445,19 @@ void *SNetRefTakeData(snet_ref_t *ref)
   return data; 
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  int SNetRefGetInterface(snet_ref_t *ref)
+ * 
+ *   @brief  Returns the language interface of the data element.
+ *
+ *   @param ref  The reference.
+ *
+ *   @return     Langauge interface id of the data.
+ *
+ ******************************************************************************/
+
 int SNetRefGetInterface(snet_ref_t *ref) 
 {
   /* No synchronization is needed as this value cannot change! */
@@ -197,10 +465,32 @@ int SNetRefGetInterface(snet_ref_t *ref)
   return ref->interface;
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn  int SNetRefPack(snet_ref_t *ref, MPI_Comm comm, int *pos, void *buf, int buf_size)
+ *
+ *   @brief  Packs presentation of the reference into a buffer with MPI_Pack.
+ *
+ *           In case of non-local data, the reference is moved to point to
+ *           the real reference (in the node where the data resides).           
+ *
+ *   @note   In case of non-local data, the call may start a remote copy operation.   
+ *
+ *   @param ref       The reference.
+ *   @param comm      The MPI communicator used to send the buffered data  (as in MPI_Pack).
+ *   @param pos       Position in the buffer (as in MPI_Pack).
+ *   @param buf       The actual buffer into which the reference is packed.
+ *   @param buf_size  The size of the buffer.
+ *
+ *   @return          MPI_SUCCESS in case of success or result of MPI_Pack otherwise.
+ *
+ ******************************************************************************/
+
 int SNetRefPack(snet_ref_t *ref, MPI_Comm comm, int *pos, void *buf, int buf_size)
 {
   int result;
-  bool b;
+  bool is_removed;
 
   pthread_mutex_lock(&ref->mutex);
 
@@ -219,19 +509,28 @@ int SNetRefPack(snet_ref_t *ref, MPI_Comm comm, int *pos, void *buf, int buf_siz
     return result;
   }
   
-  if(ref->data == NULL) {
+  if(ref->data == NULL && !ref->is_fetching) {
     if(ref->ref_count == 1) {
+
+      /* Try to remove the reference.
+       *
+       * It is possible that the reference is copied 
+       * before it is removed from the data storage.
+       * In this case the reference is not destroyed.
+       *
+       */
+
       do {
 	pthread_mutex_unlock(&ref->mutex);
 
-	b = SNetDataStorageRemove(ref->id);
+	is_removed = SNetDataStorageRemove(ref->id);
 
 	pthread_mutex_lock(&ref->mutex);
 
-      } while(!b && ref->ref_count == 1);
+      } while(!is_removed && ref->ref_count == 1);
 
 
-      if(b) {
+      if(is_removed) {
 
 	pthread_mutex_unlock(&ref->mutex);
 
@@ -258,11 +557,33 @@ int SNetRefPack(snet_ref_t *ref, MPI_Comm comm, int *pos, void *buf, int buf_siz
     }
     
   } else {
+
     pthread_mutex_unlock(&ref->mutex);
   }
 
   return MPI_SUCCESS;
 }
+
+
+/** <!--********************************************************************-->
+ *
+ * @fn  snet_ref_t *SNetRefUnpack(MPI_Comm comm, int *pos, void *buf, int buf_size)
+ *
+ *   @brief  Constructs a reference from a presentation packed with SNetRefPack
+ *
+ *           In case of non-local data, the reference is moved to point to
+ *           the real reference (in the node where the data resides).           
+ *
+ *   @note   In case of non-local data, the call may start a remote delete operation.   
+ *
+ *   @param comm      The MPI communicator used to send the buffered data  (as in MPI_Unpack).
+ *   @param pos       Position in the buffer (as in MPI_Unpack).
+ *   @param buf       The actual buffer from which the reference is unpacked.
+ *   @param buf_size  The size of the buffer.
+ *
+ *   @return          A pointer to the reference in case of success, or NULL otherwise.
+ *
+ ******************************************************************************/
 
 snet_ref_t *SNetRefUnpack(MPI_Comm comm, int *pos, void *buf, int buf_size)
 {
@@ -284,4 +605,6 @@ snet_ref_t *SNetRefUnpack(MPI_Comm comm, int *pos, void *buf, int buf_size)
 
   return SNetRefCreate(id, location, interface, NULL);
 }
+/*@}*/
+
 #endif /* DISTRIBUTED_SNET */
