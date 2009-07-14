feade31bc92490bb239e53a647262af94953d5d4
Frank Penczek <frank@penczek.eu>
2009-07-14 20:49:31 +0000

    - repaired build system for product version (sort of...) - changes to distribution build script

--- code/trunk/distbuilder/build-dist.sh
+++ code/trunk/distbuilder/build-dist.sh
@@ -5,12 +5,20 @@
 # v0.01 initial version (fpz, 13.07.09)
 #
 
-DESTDIR=/tmp/snet-dist
+# where to build the distribution
+DIST=snet-dist
+TMPDIR=/tmp
+DESTDIR=$TMPDIR/$DIST
 
+# what to delete from tree
 DEL_FROM_ROOT="Makefile config* ide src tests TODO distbuilder"
-
 EXAMPLE_DIR=$DESTDIR/examples
 DEL_FROM_EXAMPLES="crypto factorial_* matrix socrad"
+
+# misc
+VERSION=`svn info $SNETBASE | grep Revision | awk '{print $2}'`
+OS=`uname -s`
+ARCH=`uname -p`
 
 if [ -d $DESTDIR ]; then
   echo "Directory $DESTDIR exists, please delete.";
@@ -19,14 +27,18 @@ else
   svn export $SNETBASE $DESTDIR;
   echo "done"
   echo "Copying README"
-  cp $SNETBASE/distbuilder/README.binary-distribution $DESTDIR
+  cp $SNETBASE/distbuilder/README.binary-distribution $DESTDIR/README
   echo "done"
   echo "Building Compiler & Runtime...";
   cd $DESTDIR;
-  SNETBASE=$DESTDIR ./configure
-  SNETBASE=$DESTDIR make
+  SNETBASE=$DESTDIR ./configure > /dev/null
+  SNETBASE=$DESTDIR make prod > /dev/null
+  echo "Symlinking executables/libraries..."
+  ln -s $DESTDIR/bin/snetc.prod $DESTDIR/bin/snetc
+  ln -s $DESTDIR/lib/libsnet.prod.a $DESTDIR/lib/libsnet.a
+  ln -s $DESTDIR/lib/libsnetutil.prod.so $DESTDIR/lib/libsnetutil.so
   echo "Building C & SAC interfaces";
-  cd $DISTDIR/interfaces/C;
+  cd $DESTDIR/interfaces/C > /dev/null
   SNETBASE=$DESTDIR make
   echo "SKIPPING SAC4C INTERFACE"
 #  cd $DISTDIR/interfaces/SAC;
@@ -47,5 +59,7 @@ else
   mkdir $DESTDIR/src
   mkdir $DESTDIR/src/makefiles
   mv $DESTDIR/save.config.mkf $DESTDIR/src/makefiles/config.mkf
-
+  echo "Building archive..."
+  cd /tmp;
+  tar cvfz $SNETBASE/snet-v$VERSION-$OS-$ARCH.tar.gz -C $TMPDIR $DIST > /dev/null
 fi
