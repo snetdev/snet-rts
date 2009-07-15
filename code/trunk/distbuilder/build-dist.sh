2e0b513336225eba04e00079bb1a94ff5fedb4d1
Frank Penczek <frank@penczek.eu>
2009-07-15 09:47:20 +0000

    minor modification on distbuilder

--- code/trunk/distbuilder/build-dist.sh
+++ code/trunk/distbuilder/build-dist.sh
@@ -13,7 +13,7 @@ DESTDIR=$TMPDIR/$DIST
 # what to delete from tree
 DEL_FROM_ROOT="Makefile config* ide src tests TODO distbuilder"
 EXAMPLE_DIR=$DESTDIR/examples
-DEL_FROM_EXAMPLES="crypto factorial_* matrix socrad"
+DEL_FROM_EXAMPLES="crypto factorial_mpi matrix socrad"
 
 # misc
 VERSION=`svn info $SNETBASE | grep Revision | awk '{print $2}'`
@@ -26,40 +26,60 @@ else
   echo "Exporting $SNETBASE...";
   svn export $SNETBASE $DESTDIR;
   echo "done"
+  echo
   echo "Copying README"
   cp $SNETBASE/distbuilder/README.binary-distribution $DESTDIR/README
   echo "done"
-  echo "Building Compiler & Runtime...";
+  echo
+  echo "Building Compiler & Runtime (ignore svn warnings)...";
   cd $DESTDIR;
   SNETBASE=$DESTDIR ./configure > /dev/null
   SNETBASE=$DESTDIR make prod > /dev/null
-  echo "Symlinking executables/libraries..."
-  ln -s $DESTDIR/bin/snetc.prod $DESTDIR/bin/snetc
-  ln -s $DESTDIR/lib/libsnet.prod.a $DESTDIR/lib/libsnet.a
-  ln -s $DESTDIR/lib/libsnetutil.prod.so $DESTDIR/lib/libsnetutil.so
+  echo "done"
+  echo
+  echo "Renaming/copying executables/libraries..."
+  mv $DESTDIR/bin/snetc.prod $DESTDIR/bin/snetc
+  cp $DESTDIR/lib/libsnet.prod.a $DESTDIR/lib/libsnet.a
+  cp $DESTDIR/lib/libsnetutil.prod.so $DESTDIR/lib/libsnetutil.so
+  echo "done"
+  echo
   echo "Building C & SAC interfaces";
-  cd $DESTDIR/interfaces/C > /dev/null
-  SNETBASE=$DESTDIR make
+  cd $DESTDIR/interfaces/C 
+  SNETBASE=$DESTDIR make > /dev/null
   echo "SKIPPING SAC4C INTERFACE"
+  echo "done"
+  echo
 #  cd $DISTDIR/interfaces/SAC;
 #  SNETBASE=$DESTDIR make
   echo "Saving config.mkf"
   cp $DESTDIR/src/makefiles/config.mkf $DESTDIR/save.config.mkf
+  echo "done"
+  echo
   echo "Purging developer related contents..."
   for F in $DEL_FROM_ROOT; do
-    echo "Deleting $DESTDIR/$F"
+    echo " - Deleting $DESTDIR/$F"
     rm -rf $DESTDIR/$F
   done
+  echo done
+  echo
   echo "Purging unwanted examples..."
   for F in $DEL_FROM_EXAMPLES; do
-    echo "Deleting $EXAMPLE_DIR/$F"
+    echo " - Deleting $EXAMPLE_DIR/$F"
     rm -rf $EXAMPLE_DIR/$F
   done
+  echo "done"
+  echo
   echo "Restoring config.mkf into original destination";
   mkdir $DESTDIR/src
   mkdir $DESTDIR/src/makefiles
   mv $DESTDIR/save.config.mkf $DESTDIR/src/makefiles/config.mkf
+  echo "done"
+  echo
   echo "Building archive..."
   cd /tmp;
   tar cvfz $SNETBASE/snet-v$VERSION-$OS-$ARCH.tar.gz -C $TMPDIR $DIST > /dev/null
+  echo
+  echo "Done."
+  echo "Archive saved to: $SNETBASE/snet-v$VERSION-$OS-$ARCH.tar.gz"
+  echo
 fi
