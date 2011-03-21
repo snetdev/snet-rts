dbd4d730d13f4e5a4c3aaab9dfedde81735d9878
Merijn Verstraaten <merijn@inconsistent.nl>
2011-03-21 15:58:43 +0100

    Updated snet_info_t implementation (i.e. should no longer deadlock).

--- code/trunk/src/runtime_lpel/shared/info.c
+++ code/trunk/src/runtime_lpel/shared/info.c
@@ -1,35 +1,25 @@
 #include <pthread.h>
+#include "arch/atomic.h"
 #include "info.h"
 #include "bool.h"
 #include "memfun.h"
 
-static snet_info_tag_t new_tag = 0;
-static bool mutex_initialized = false;
-static pthread_mutex_t info_mutex;
+static atomic_t new_tag = ATOMIC_INIT(0);
 
 struct snet_info {
-    snet_info_tag_t tag;
-    void *data;
-    struct snet_info *next;
+  pthread_mutex_t mutex;
+  snet_info_tag_t tag;
+  void *data;
+  struct snet_info *next;
 };
 
 snet_info_tag_t SNetInfoCreateTag()
 {
-    int result;
-    pthread_mutex_lock(&info_mutex);
-    result = new_tag++;
-    pthread_mutex_unlock(&info_mutex);
-    return result;
+    return fetch_and_inc(&new_tag);
 }
 
-snet_info_t *SNetInfoInit()
+static snet_info_t *NewInfo()
 {
-  if (!mutex_initialized) {
-  //FIXME: error handling?
-    pthread_mutex_init(&info_mutex, NULL);
-    mutex_initialized = true;
-  }
-
   snet_info_t *new = SNetMemAlloc(sizeof(*new));
   new->tag = -1;
   new->data = NULL;
@@ -37,9 +27,16 @@ snet_info_t *SNetInfoInit()
   return new;
 }
 
+snet_info_t *SNetInfoInit()
+{
+  snet_info_t *new = NewInfo();
+  pthread_mutex_init(&new->mutex, NULL);
+  return new;
+}
+
 void SNetInfoSetTag(snet_info_t *info, snet_info_tag_t tag, void *data)
 {
-  pthread_mutex_lock(&info_mutex);
+  pthread_mutex_lock(&info->mutex);
   while (info->tag != tag && info->tag != -1) {
       info = info->next;
   }
@@ -49,9 +46,9 @@ void SNetInfoSetTag(snet_info_t *info, snet_info_tag_t tag, void *data)
   } else if (info->tag == -1) {
       info->tag = tag;
       info->data = data;
-      info->next = SNetInfoInit();
+      info->next = NewInfo();
   }
-  pthread_mutex_unlock(&info_mutex);
+  pthread_mutex_unlock(&info->mutex);
 }
 
 void *SNetInfoDelTag(snet_info_t *info, snet_info_tag_t tag)
@@ -59,7 +56,7 @@ void *SNetInfoDelTag(snet_info_t *info, snet_info_tag_t tag)
   void *result;
   snet_info_t *tmp;
 
-  pthread_mutex_lock(&info_mutex);
+  pthread_mutex_lock(&info->mutex);
 
   while (info->next != NULL && info->next->tag != tag) {
       info = info->next;
@@ -69,48 +66,48 @@ void *SNetInfoDelTag(snet_info_t *info, snet_info_tag_t tag)
 
   tmp = info->next;
   info->next = tmp->next;
-  result = tmp->next;
+  result = tmp->data;
   SNetMemFree(tmp);
-  pthread_mutex_unlock(&info_mutex);
+  pthread_mutex_unlock(&info->mutex);
   return result;
 }
 
 void *SNetInfoGetTag(snet_info_t *info, snet_info_tag_t tag)
 {
   void *result = NULL;
-  pthread_mutex_lock(&info_mutex);
+  pthread_mutex_lock(&info->mutex);
   while (info->tag != tag && info->tag != -1) {
       info = info->next;
   }
 
   if (info->tag == tag) result = info->data;
-  pthread_mutex_unlock(&info_mutex);
+  pthread_mutex_unlock(&info->mutex);
   return result;
 }
 
 snet_info_t *SNetInfoCopy(snet_info_t *info) {
-    snet_info_t *tmp, *result;
-    pthread_mutex_lock(&info_mutex);
-    tmp = result = SNetMemAlloc(sizeof(snet_info_t));
+    snet_info_t *tmp, *tmp2, *result;
+    pthread_mutex_lock(&info->mutex);
+    tmp = result = SNetInfoInit();
+    tmp2 = info;
     do {
-        tmp->tag = info->tag;
-        tmp->data = info->data;
-        tmp->next = info->next ? SNetMemAlloc(sizeof(snet_info_t)) : NULL;
+        tmp->tag = tmp2->tag;
+        tmp->data = tmp2->data;
+        tmp->next = tmp2->next ? NewInfo() : NULL;
         tmp = tmp->next;
-        info = info->next;
-    } while (info != NULL);
-    pthread_mutex_unlock(&info_mutex);
+        tmp2 = tmp2->next;
+    } while (tmp2 != NULL);
+    pthread_mutex_unlock(&info->mutex);
     return result;
 }
 
 void SNetInfoDestroy(snet_info_t *info)
 {
-  pthread_mutex_lock(&info_mutex);
+  pthread_mutex_destroy(&info->mutex);
   snet_info_t *tmp;
   while (info != NULL) {
     tmp = info->next;
     SNetMemFree(info);
     info = tmp;
   }
-  pthread_mutex_unlock(&info_mutex);
 }
