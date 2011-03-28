86ba1fd064cf05c35d8c57307b4e1beae0db7265
Daniel Prokesch <daniel.prokesch@gmail.com>
2011-03-28 11:03:42 +0200

    LPEL backend now compiled by translating the LPEL prefixes to SNet from the LPEL sources instead of indirect calling of LPEL functions with casted parameters

--- code/trunk/src/threading/lpel/glue_snet.c
+++ code/trunk/src/threading/lpel/glue_snet.c
@@ -10,7 +10,7 @@
 #include "threading.h"
 
 /* LPEL library interface */
-#include "lpel.h"
+#include "lpel4snet.h"
 
 /* provisional assignment module */
 #include "assign.h"
@@ -54,12 +54,12 @@ static size_t SNetEntityStackSize(snet_entity_type_t type)
 
 int SNetThreadingInit(int argc, char **argv)
 {
-  lpel_config_t config;
+  snet_config_t config;
   char fname[20+1];
   int num_cpus, num_workers=0;
   int i;
 
-  config.flags = LPEL_FLAG_PINNED;
+  config.flags = SNET_FLAG_PINNED;
   config.node = SNetNodeLocation;
   
   for (i=0; i<argc; i++) {
@@ -69,7 +69,7 @@ int SNetThreadingInit(int argc, char **argv)
       mon_level = atoi(argv[i]);
     } else if(strcmp(argv[i], "-excl") == 0 ) {
       /* Assign realtime priority to workers*/
-      config.flags |= LPEL_FLAG_EXCLUSIVE;
+      config.flags |= SNET_FLAG_EXCLUSIVE;
     } else if(strcmp(argv[i], "-w") == 0 && i + 1 <= argc) {
       /* Number of workers */
       i = i + 1;
@@ -90,7 +90,7 @@ int SNetThreadingInit(int argc, char **argv)
   }
 
   /* determine number of cpus */
-  if ( 0 != LpelGetNumCores( &num_cpus) ) {
+  if ( 0 != SNetGetNumCores( &num_cpus) ) {
     //FIXME SNetUtilDebugFatal("Could not determine number of cores!\n");
     assert(0);
   }
@@ -109,7 +109,7 @@ int SNetThreadingInit(int argc, char **argv)
 
   SNetAssignInit(config.num_workers);
 
-  LpelInit(&config);
+  SNetInit(&config);
 
   return 0;
 }
@@ -117,14 +117,14 @@ int SNetThreadingInit(int argc, char **argv)
 
 void SNetThreadingStop(void)
 {
-  LpelStop();
+  SNetStop();
 }
 
 
 int SNetThreadingProcess(void)
 {
   /* following call will block until the workers have finished */
-  LpelCleanup();
+  SNetCleanup();
   return 0;
 }
 
@@ -149,9 +149,9 @@ int SNetEntitySpawn(snet_entity_info_t info, snet_entityfunc_t func, void *arg)
   int do_mon = 0;
   int worker = SNetAssignTask( (info.type==ENTITY_box), info.name );
 
-  lpel_task_t *t = LpelTaskCreate(
+  snet_entity_t *t = SNetEntityCreate(
       worker,
-      (lpel_taskfunc_t) func,
+      (snet_entityfunc_t) func,
       arg,
       SNetEntityStackSize(info.type)
       );
@@ -165,18 +165,18 @@ int SNetEntitySpawn(snet_entity_info_t info, snet_entityfunc_t func, void *arg)
   
   mon_flags = 0;
   switch(mon_level) {
-    case 4: mon_flags |= LPEL_MON_TASK_STREAMS;
-    case 3: mon_flags |= LPEL_MON_TASK_TIMES;
+    case 4: mon_flags |= SNET_MON_TASK_STREAMS;
+    case 3: mon_flags |= SNET_MON_TASK_TIMES;
     case 2:
       if (info.type==ENTITY_box) {
-        LpelTaskMonitor(t, info.name, mon_flags);
+        SNetEntityMonitor(t, info.name, mon_flags);
         do_mon = 1;
       }
       break;
 
     case 5:
       if (info.type!=ENTITY_other) {
-        LpelTaskMonitor(t, info.name, LPEL_MON_TASK_STREAMS | LPEL_MON_TASK_TIMES );
+        SNetEntityMonitor(t, info.name, SNET_MON_TASK_STREAMS | SNET_MON_TASK_TIMES );
         do_mon = 1;
       }
       break;
@@ -184,182 +184,12 @@ int SNetEntitySpawn(snet_entity_info_t info, snet_entityfunc_t func, void *arg)
   }
 
   if (do_mon && mapfile) {
-    int tid = LpelTaskGetUID(t);
+    int tid = SNetEntityGetUID(t);
     (void) fprintf(mapfile, "%d: %s\n", tid, info.name);
   }
 
-  LpelTaskRun(t);
+  SNetEntityRun(t);
   return 0;
-}
-
-
-
-
-/**
- * Let the current entity give up execution
- */
-void SNetEntityYield(snet_entity_t *self)
-{
-  LpelTaskYield( (lpel_task_t *)self );
-}
-
-
-/**
- * Let the current entity exit
- */
-void SNetEntityExit(snet_entity_t *self)
-{
-  LpelTaskExit( (lpel_task_t *)self );
-}
-
-
-
-
-
-snet_stream_t *SNetStreamCreate(int capacity)
-{
-  return (snet_stream_t *)LpelStreamCreate(capacity);
-}
-
-
-
-snet_stream_desc_t *SNetStreamOpen(snet_entity_t *entity,
-    snet_stream_t *stream, char mode)
-{
-  return (snet_stream_desc_t *) LpelStreamOpen(
-      (lpel_task_t *) entity,
-      (lpel_stream_t *) stream,
-      mode
-      );
-}
-
-
-void SNetStreamClose(snet_stream_desc_t *sd, int destroy_stream)
-{
-  LpelStreamClose(
-      (lpel_stream_desc_t *)sd,
-      destroy_stream
-      );
-}
-
-
-
-
-void SNetStreamReplace(snet_stream_desc_t *sd, snet_stream_t *new_stream)
-{
-  LpelStreamReplace(
-      (lpel_stream_desc_t *) sd,
-      (lpel_stream_t *) new_stream
-      );
-}
-
-
-void *SNetStreamRead(snet_stream_desc_t *sd)
-{
-  return LpelStreamRead( (lpel_stream_desc_t *)sd );
-}
-
-
-void *SNetStreamPeek(snet_stream_desc_t *sd)
-{
-  return LpelStreamPeek( (lpel_stream_desc_t *)sd );
-}
-
-
-void SNetStreamWrite(snet_stream_desc_t *sd, void *item)
-{
-  return LpelStreamWrite( (lpel_stream_desc_t *)sd , item );
-}
-
-
-int SNetStreamTryWrite(snet_stream_desc_t *sd, void *item)
-{
-  //FIXME
-  assert(0);
-  return 0;
-}
-
-
-snet_stream_desc_t *SNetStreamPoll(snet_streamset_t *set)
-{
-  return (snet_stream_desc_t *) LpelStreamPoll(
-      (lpel_streamset_t *) set
-      );
-}
-
-
-
-void SNetStreamsetPut(snet_streamset_t *set, snet_stream_desc_t *sd)
-{
-  LpelStreamsetPut(
-      (lpel_streamset_t *) set,
-      (lpel_stream_desc_t *) sd
-      );
-}
-
-
-
-int SNetStreamsetRemove(snet_streamset_t *set, snet_stream_desc_t *sd)
-{
-  return LpelStreamsetRemove(
-      (lpel_streamset_t *) set,
-      (lpel_stream_desc_t *) sd
-      );
-}
-
-
-
-snet_stream_iter_t *SNetStreamIterCreate(snet_streamset_t *set)
-{
-  return (snet_stream_iter_t *) LpelStreamIterCreate(
-      (lpel_streamset_t *) set
-      );
-}
-
-
-
-void SNetStreamIterDestroy(snet_stream_iter_t *iter)
-{
-  LpelStreamIterDestroy( (lpel_stream_iter_t *) iter );
-}
-
-
-
-void SNetStreamIterReset(snet_stream_iter_t *iter, snet_streamset_t *set)
-{
-  LpelStreamIterReset(
-      (lpel_stream_iter_t *) iter,
-      (lpel_streamset_t *) set
-      );
-}
-
-
-
-int SNetStreamIterHasNext(snet_stream_iter_t *iter)
-{
-  return LpelStreamIterHasNext( (lpel_stream_iter_t *) iter );
-}
-
-
-snet_stream_desc_t *SNetStreamIterNext( snet_stream_iter_t *iter)
-{
-  return (snet_stream_desc_t *) LpelStreamIterNext(
-      (lpel_stream_iter_t *) iter
-      );
-}
-
-
-void SNetStreamIterAppend( snet_stream_iter_t *iter, snet_stream_desc_t *node)
-{
-  LpelStreamIterAppend(
-      (lpel_stream_iter_t *) iter,
-      (lpel_stream_desc_t *) node
-      );
-}
-
-void SNetStreamIterRemove( snet_stream_iter_t *iter)
-{
-  LpelStreamIterRemove( (lpel_stream_iter_t *) iter );
 }
 
 
---
+++ code/trunk/src/threading/lpel/translate4snet.sed
@@ -0,0 +1,8 @@
+# sed script file for translating symbols from LPEL
+# to the SNet threading backend
+s/lpel_task/snet_entity/g
+s/LpelTask/SNetEntity/g
+s/lpel_/snet_/g
+s/Lpel/SNet/g
+s/_LPEL_H_/_LPEL4SNET_H_/g
+s/LPEL/SNET/g
