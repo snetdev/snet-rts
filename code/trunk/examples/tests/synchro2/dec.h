8c2b89feb9ca5ec5e61bad9fe7731927594fe908
Daniel Prokesch <daniel.prokesch@gmail.com>
2011-04-22 11:27:22 +0200

    Added example to synchronize up with state (examples/tests/synchro2).

---
+++ code/trunk/examples/tests/synchro2/dec.h
@@ -0,0 +1,10 @@
+#ifndef SUB_H_
+#define SUB_H_ 
+
+#include <C4SNet.h>
+
+void *dec( void *hnd, c4snet_data_t *state, c4snet_data_t *inval);
+
+/*void *dec( void *hnd, c4snet_data_t *b);*/
+#endif /* SUB_H_ */
+
