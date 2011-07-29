581a2a8bd990ebd8cb23998bf9ea63657d7c17b2
Raimund Kirner <r.kirner@herts.ac.uk>
2011-07-29 17:19:55 +0300

    creation of monitoring information files.

---
+++ code/trunk/include/moninfo.h
@@ -0,0 +1,22 @@
+/*
+ * moninfo.h
+ *
+ *  Created on: Jul 29, 2011
+ *      Author: Raimund Kirner
+ * This file includes data types and functions to maintain monitoring information.
+ * The main purpose at creation (July 2011) was to have monitoring information for
+ * each S-Net record to log the overall execution time in a (sub)network from reading
+ * a record until the output of all of its descendant records at the exit of this
+ * (sub)network.
+ *
+ */
+
+#ifndef MONINFO_H_
+#define MONINFO_H_
+
+typedef int[3] record_id_t;  /* fields: 0..id (unique for thread), 1..thread_id, 2..node_id */
+
+
+
+
+#endif /* MONINFO_H_ */
---
+++ code/trunk/src/runtime/common/moninfo.c
@@ -0,0 +1,19 @@
+/*
+ * moninfo.c
+ *
+ *  Created on: 29. July  2011
+ *      Author: Raimund Kirner
+ *
+ * This file includes data types and functions to maintain monitoring information.
+ * The main purpose at creation (July 2011) was to have monitoring information for
+ * each S-Net record to log the overall execution time in a (sub)network from reading
+ * a record until the output of all of its descendant records at the exit of this
+ * (sub)network.
+ *
+ */
+
+#include "moninfo.h"
+
+
+
+
