101e3221918613ca82e8e4ce0d6dcc8d4474a9db
Merijn Verstraaten <merijn@inconsistent.nl>
2011-07-30 15:42:03 +0300

    !! DIRTY HACK ALERT!! Dynamic network support for the MPI distribution layer.

--- code/trunk/src/distrib/mpi/distribmap.h
+++ code/trunk/src/distrib/mpi/distribmap.h
@@ -7,13 +7,15 @@
 typedef struct {
   int node,
       dest,
-      dynamicParent;
+      parent,
+      parentNode,
+      parentIndex;
 } snet_dest_t;
 
 #define MAP_NAME_H DestStream
 #define MAP_TYPE_NAME_H dest_stream
 #define MAP_KEY_H snet_dest_t
-#define MAP_VAL_H snet_stream_desc_t*
+#define MAP_VAL_H void*
 #include "map-template.h"
 #undef MAP_VAL_H
 #undef MAP_KEY_H
@@ -22,11 +24,19 @@ typedef struct {
 
 #define MAP_NAME_H StreamDest
 #define MAP_TYPE_NAME_H stream_dest
-#define MAP_KEY_H snet_stream_desc_t*
+#define MAP_KEY_H void*
 #define MAP_VAL_H snet_dest_t
 #include "map-template.h"
 #undef MAP_VAL_H
 #undef MAP_KEY_H
+#undef MAP_TYPE_NAME_H
+#undef MAP_NAME_H
+
+#define MAP_NAME_H Dynamic
+#define MAP_TYPE_NAME_H dynamic
+#define MAP_VAL_H snet_startup_fun_t
+#include "map-template.h"
+#undef MAP_VAL_H
 #undef MAP_TYPE_NAME_H
 #undef MAP_NAME_H
 
--- code/trunk/src/distrib/mpi/imanager.c
+++ code/trunk/src/distrib/mpi/imanager.c
@@ -1,62 +1,82 @@
+#include <assert.h>
 #include <mpi.h>
+#include <stdio.h>
+#include "info.h"
 #include "memfun.h"
+#include "distribution.h"
 #include "distribmap.h"
 
-extern snet_dest_stream_map_t *destMap;
-extern int node_location;
-
+static int offset;
+static size_t size = 1000;
+static char buf[1000];
 static bool running = true;
+extern int node_location;
+extern bool outputDistribInfo;
+extern snet_dynamic_map_t *dynamicMap;
+extern snet_dest_stream_map_t *destMap;
+extern snet_info_tag_t prevDest;
+extern snet_stream_desc_t *sd;
+extern void *SNetDestCopy(void *d);
+
+static void MPIUnpackWrapper(int foo, int *bar)
+{
+  MPI_Unpack(buf, size, &offset, bar, foo, MPI_INT, MPI_COMM_WORLD);
+}
 
 void RecvRecord(MPI_Status status)
 {
-  snet_stream_desc_t *sd;
-  snet_dest_t dest = {node_location, status.MPI_TAG, 0};
-  snet_record_t *rec = SNetRecCreate(REC_data);
-  int tmp[2];
-  int count;
-  size_t size = 1000;
-  char *buf = SNetMemAlloc(size);
+  snet_stream_desc_t *sdc;
+  snet_dest_t dest = {SNetDistribGetNodeId(), 0, status.MPI_TAG, 0, 0};
+  snet_record_t *rec;
 
-  MPI_Get_count(&status, MPI_PACKED, &count);
-  MPI_Recv(buf, count, MPI_PACKED, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+  MPI_Get_count(&status, MPI_PACKED, &offset);
+  if (status.MPI_TAG == 0) {
+    MPI_Recv(buf, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+    SNetStreamWrite(sd, NULL);
+    assert(SNetDestStreamMapSize(destMap) == 0);
+    running = false;
+    return;
+  }
+  MPI_Recv(buf, offset, MPI_PACKED, MPI_ANY_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
 
-  count = 0;
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, tags)->size, 1, MPI_INT, MPI_COMM_WORLD);
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, tags)->used, 1, MPI_INT, MPI_COMM_WORLD);
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, btags)->size, 1, MPI_INT, MPI_COMM_WORLD);
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, btags)->used, 1, MPI_INT, MPI_COMM_WORLD);
-  MPI_Unpack(buf, size, &count, tmp, 1, MPI_INT, MPI_COMM_WORLD);
-  DATA_REC(rec, mode) = tmp[0];
+  offset = 0;
+  MPIUnpackWrapper(1, &dest.dest);
+  MPIUnpackWrapper(1, &dest.parentIndex);
+  MPIUnpackWrapper(1, &dest.parentNode);
+  rec = SNetRecDeserialise(&MPIUnpackWrapper);
 
-  DATA_REC(rec, tags)->keys = SNetMemAlloc(DATA_REC(rec, tags)->size * sizeof(int));
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, tags)->keys, DATA_REC(rec, tags)->size, MPI_INT, MPI_COMM_WORLD);
+  //fprintf(stderr, "#%d: Recieved message for: %d, %d, %d, %d\n", node_location, dest.node, dest.dest, dest.parent, dest.parentIndex);
+  //fflush(stderr);
+  if (!SNetDestStreamMapContains(destMap, dest)) {
+    snet_dest_t *dst = SNetMemAlloc(sizeof(snet_dest_t));
+    *dst = dest;
+    dst->node = status.MPI_SOURCE;
+    snet_startup_fun_t fun = SNetDynamicMapGet(dynamicMap, dest.parent);
 
-  DATA_REC(rec, tags)->values = SNetMemAlloc(DATA_REC(rec, tags)->size * sizeof(int));
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, tags)->values, DATA_REC(rec, tags)->size, MPI_INT, MPI_COMM_WORLD);
-
-  DATA_REC(rec, btags)->keys = SNetMemAlloc(DATA_REC(rec, tags)->size * sizeof(int));
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, btags)->keys, DATA_REC(rec, btags)->size, MPI_INT, MPI_COMM_WORLD);
-
-  DATA_REC(rec, btags)->values = SNetMemAlloc(DATA_REC(rec, tags)->size * sizeof(int));
-  MPI_Unpack(buf, size, &count, &DATA_REC(rec, btags)->values, DATA_REC(rec, btags)->size, MPI_INT, MPI_COMM_WORLD);
-
-  sd = SNetDestStreamMapGet(destMap, dest);
-  SNetStreamWrite(sd, rec);
-  SNetMemFree(buf);
+    snet_info_t *info = SNetInfoInit();
+    SNetInfoSetTag(info, prevDest, (uintptr_t) dst, &SNetDestCopy);
+    SNetLocvecSet(info, SNetLocvecCreate());
+    snet_stream_t *str = SNetStreamCreate(0);
+    SNetDestStreamMapSet(destMap, dest, SNetStreamOpen(str, 'w'));
+    SNetRouteUpdateDynamic(info, str, node_location, dest.parentIndex);
+    str = fun(str, info, node_location);
+    str = SNetRouteUpdate(info, str, dest.parentNode, NULL);
+    SNetInfoDestroy(info);
+  }
+  if (REC_DESCR(rec) == REC_terminate) {
+    sdc = SNetDestStreamMapTake(destMap, dest);
+  } else {
+    sdc = SNetDestStreamMapGet(destMap, dest);
+  }
+  SNetStreamWrite(sdc, rec);
 }
 
-void *SNetInputManager(void *args)
+void SNetInputManager(void *args)
 {
   MPI_Status status;
 
   while (running) {
     MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    switch (status.MPI_TAG) {
-      default:
-        /*assert(0); //Unknown message*/
-        RecvRecord(status);
-        break;
-    }
+    RecvRecord(status);
   }
-  return NULL;
 }
