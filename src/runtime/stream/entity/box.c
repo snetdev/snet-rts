ed350efff0d97eb5971e6981852770ffedeeed9b
Frank Penczek <frank@penczek.eu>
2012-06-08 10:44:06 +0100

    removed spaces and other trivial changes

--- src/runtime/stream/entity/box.c
+++ src/runtime/stream/entity/box.c
@@ -16,9 +16,6 @@
 #include "debugcounters.h"
 #endif /* SNET_DEBUG_COUNTERS  */
 
-
-//#define DBG_RT_TRACE_BOX_TIMINGS
-
 #ifdef DBG_RT_TRACE_BOX_TIMINGS
 #include <sys/time.h>
 #endif
@@ -27,7 +24,7 @@
 
 typedef struct {
   snet_stream_t *input, *output;
-  void (*boxfun)( snet_handle_t*);
+  void (*boxfun)(snet_handle_t*);
   snet_int_list_list_t *output_variants;
   const char *boxname;
   snet_variant_list_t *vars;
@@ -75,18 +72,18 @@ static void BoxTask(snet_entity_t *ent, void *arg)
   hnd.ent = ent;
 
   /* MAIN LOOP */
-  while( !terminate) {
+  while(!terminate) {
     /* read from input stream */
-    rec = SNetStreamRead( instream);
+    rec = SNetStreamRead(instream);
 
-    switch( SNetRecGetDescriptor(rec)) {
+    switch(SNetRecGetDescriptor(rec)) {
       case REC_trigger_initialiser:
       case REC_data:
         hnd.rec = rec;
 
 #ifdef DBG_RT_TRACE_BOX_TIMINGS
-        gettimeofday( &tv_in, NULL);
-        SNetUtilDebugNoticeEnt( ent,
+        gettimeofday(&tv_in, NULL);
+        SNetUtilDebugNoticeEnt(ent,
             "[BOX] Firing box function at %lf.",
             tv_in.tv_sec + tv_in.tv_usec / 1000000.0
             );
@@ -98,22 +95,22 @@ static void BoxTask(snet_entity_t *ent, void *arg)
 #ifdef USE_USER_EVENT_LOGGING
         /* Emit a monitoring message of a record read to be processed by a box */
         if (SNetRecGetDescriptor(rec) == REC_data) {
-          SNetThreadingEventSignal( ent,
-              SNetMonInfoCreate( EV_MESSAGE_IN, MON_RECORD, rec)
+          SNetThreadingEventSignal(ent,
+              SNetMonInfoCreate(EV_MESSAGE_IN, MON_RECORD, rec)
               );
         }
 #endif
 
-        (*barg->boxfun)( &hnd);
+        (*barg->boxfun)(&hnd);
 
         /*
          * Emit an event here?
-         * SNetMonInfoEvent( EV_BOX_???, MON_RECORD, rec);
+         * SNetMonInfoEvent(EV_BOX_???, MON_RECORD, rec);
          */
 
 #ifdef DBG_RT_TRACE_BOX_TIMINGS
-        gettimeofday( &tv_out, NULL);
-        SNetUtilDebugNoticeEnt( ent,
+        gettimeofday(&tv_out, NULL);
+        SNetUtilDebugNoticeEnt(ent,
             "[BOX] Return from box function after %lf sec.",
             (tv_out.tv_sec - tv_in.tv_sec) + (tv_out.tv_usec - tv_in.tv_usec) / 1000000.0
             );
@@ -126,7 +123,7 @@ static void BoxTask(snet_entity_t *ent, void *arg)
         SNetDebugCountersIncreaseCounter(mseconds, SNET_COUNTER_TIME_BOX);
 #endif /* SNET_DEBUG_COUNTERS */
 
-        SNetRecDestroy( rec);
+        SNetRecDestroy(rec);
 
         /* restrict to one data record per execution */
         //SNetThreadingYield();
@@ -135,18 +132,18 @@ static void BoxTask(snet_entity_t *ent, void *arg)
       case REC_sync:
         {
           snet_stream_t *newstream = SNetRecGetStream(rec);
-          SNetStreamReplace( instream, newstream);
-          SNetRecDestroy( rec);
+          SNetStreamReplace(instream, newstream);
+          SNetRecDestroy(rec);
         }
         break;
 
       case REC_sort_end:
         /* forward the sort record */
-        SNetStreamWrite( outstream, rec);
+        SNetStreamWrite(outstream, rec);
         break;
 
       case REC_terminate:
-        SNetStreamWrite( outstream, rec);
+        SNetStreamWrite(outstream, rec);
         terminate = true;
         break;
 
@@ -154,18 +151,18 @@ static void BoxTask(snet_entity_t *ent, void *arg)
       default:
         assert(0);
         /* if ignore, destroy at least ...*/
-        SNetRecDestroy( rec);
+        SNetRecDestroy(rec);
     }
   } /* MAIN LOOP END */
 
-  SNetStreamClose( instream, true);
-  SNetStreamClose( outstream, false);
+  SNetStreamClose(instream, true);
+  SNetStreamClose(outstream, false);
 
-  SNetHndDestroy( &hnd);
+  SNetHndDestroy(&hnd);
 
   /* destroy box arg */
   SNetIntListListDestroy(barg->output_variants);
-  SNetMemFree( barg);
+  SNetMemFree(barg);
 }
 
 
@@ -173,7 +170,7 @@ static void BoxTask(snet_entity_t *ent, void *arg)
 /**
  * Box creation function
  */
-snet_stream_t *SNetBox( snet_stream_t *input,
+snet_stream_t *SNetBox(snet_stream_t *input,
                         snet_info_t *info,
                         int location,
                         const char *boxname,
@@ -190,28 +187,28 @@ snet_stream_t *SNetBox( snet_stream_t *input,
   if(SNetDistribIsNodeLocation(location)) {
     output = SNetStreamCreate(0);
     vlist = SNetVariantListCreate(0);
-    for( i=0; i<SNetIntListListLength( output_variants); i++) {
-      snet_int_list_t *l = SNetIntListListGet( output_variants, i);
+    for(i=0; i<SNetIntListListLength(output_variants); i++) {
+      snet_int_list_t *l = SNetIntListListGet(output_variants, i);
       snet_variant_t *v = SNetVariantCreateEmpty();
-      for( j=0; j<SNetIntListLength( l); j+=2) {
-        switch( SNetIntListGet( l, j)) {
+      for(j=0; j<SNetIntListLength(l); j+=2) {
+        switch(SNetIntListGet(l, j)) {
           case field:
-            SNetVariantAddField( v, SNetIntListGet( l, j+1));
+            SNetVariantAddField(v, SNetIntListGet(l, j+1));
             break;
           case tag:
-            SNetVariantAddTag( v, SNetIntListGet( l, j+1));
+            SNetVariantAddTag(v, SNetIntListGet(l, j+1));
             break;
           case btag:
-            SNetVariantAddBTag( v, SNetIntListGet( l, j+1));
+            SNetVariantAddBTag(v, SNetIntListGet(l, j+1));
             break;
           default:
             assert(0);
         }
       }
-      SNetVariantListAppendEnd( vlist, v);
+      SNetVariantListAppendEnd(vlist, v);
     }
 
-    barg = (box_arg_t *) SNetMemAlloc( sizeof( box_arg_t));
+    barg = (box_arg_t *) SNetMemAlloc(sizeof(box_arg_t));
     barg->input  = input;
     barg->output = output;
     barg->boxfun = boxfun;
@@ -220,7 +217,7 @@ snet_stream_t *SNetBox( snet_stream_t *input,
     barg->boxname = boxname;
 
     SNetThreadingSpawn(
-        SNetEntityCreate( ENTITY_box, location, SNetLocvecGet(info),
+        SNetEntityCreate(ENTITY_box, location, SNetLocvecGet(info),
           barg->boxname, BoxTask, (void*)barg)
         );
 
--- src/runtime/stream/shared/handle.c
+++ src/runtime/stream/shared/handle.c
@@ -11,58 +11,58 @@
 #include "bool.h"
 
 
-void SNetHndDestroy( snet_handle_t *hnd) {
+void SNetHndDestroy(snet_handle_t *hnd) {
 
   int i;
-  if( hnd->mapping != NULL) {
-    for( i=0; i<hnd->mapping->num; i++) {
-      SNetMemFree( hnd->mapping->string_names[i]);
+  if(hnd->mapping != NULL) {
+    for(i=0; i<hnd->mapping->num; i++) {
+      SNetMemFree(hnd->mapping->string_names[i]);
     }
-    SNetMemFree( hnd->mapping->string_names);
-    SNetMemFree( hnd->mapping->int_names);
+    SNetMemFree(hnd->mapping->string_names);
+    SNetMemFree(hnd->mapping->int_names);
   }
-  SNetVariantListDestroy( hnd->vars);
+  SNetVariantListDestroy(hnd->vars);
 }
 
 /**
  * Should be public
  */
-snet_record_t *SNetHndGetRecord( snet_handle_t *hnd)
+snet_record_t *SNetHndGetRecord(snet_handle_t *hnd)
 { 
   return hnd->rec;
 }
 
-snet_int_list_list_t *SNetHndGetVariants( snet_handle_t *hnd)
+snet_int_list_list_t *SNetHndGetVariants(snet_handle_t *hnd)
 {
   return hnd->sign;
 }
 
-snet_variant_list_t *SNetHndGetVariantList( snet_handle_t *hnd)
+snet_variant_list_t *SNetHndGetVariantList(snet_handle_t *hnd)
 {
-  return( hnd->vars);
+  return(hnd->vars);
 }
 
-void SNetHndSetStringNames( snet_handle_t *hnd, int num, ...)
+void SNetHndSetStringNames(snet_handle_t *hnd, int num, ...)
 {
   int i;
   va_list args;
 
-  va_start( args, num);
+  va_start(args, num);
  
-  hnd->mapping = (name_mapping_t *) SNetMemAlloc( sizeof( name_mapping_t));
+  hnd->mapping = (name_mapping_t *) SNetMemAlloc(sizeof(name_mapping_t));
   hnd->mapping->num = num;
-  hnd->mapping->int_names = (int *) SNetMemAlloc( num * sizeof( int));
-  hnd->mapping->string_names = (char **) SNetMemAlloc( num * sizeof( char*));
+  hnd->mapping->int_names = (int *) SNetMemAlloc(num * sizeof(int));
+  hnd->mapping->string_names = (char **) SNetMemAlloc(num * sizeof(char*));
 
-  for( i=0; i<num; i++) {
+  for(i=0; i<num; i++) {
     char *str;
     int len;
 
-    hnd->mapping->int_names[i] = va_arg( args, int);
-    str = va_arg( args, char*);
+    hnd->mapping->int_names[i] = va_arg(args, int);
+    str = va_arg(args, char*);
     len = strlen(str)+1;
-    hnd->mapping->string_names[i] = (char *) SNetMemAlloc( len * sizeof( char));
-    (void) strncpy( hnd->mapping->string_names[i], str, len);
+    hnd->mapping->string_names[i] = (char *) SNetMemAlloc(len * sizeof(char));
+    (void) strncpy(hnd->mapping->string_names[i], str, len);
     hnd->mapping->string_names[i][len-1] = '\0';
   }
 }
