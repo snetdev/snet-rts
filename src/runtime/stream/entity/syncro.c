749b00237b46b0e0327c4d4ff4f0f182fc7a59bc
Nga Nguyen <nguyenvuthiennga@gmail.com>
2012-10-10 18:03:21 +0100

    fix bug of repeating message IDs  - reason: SNetRecCopy copy a data record without creat a new    record/message ID. This function is called by split and sync  - in syncro.c: clean up the code a bit

--- src/runtime/stream/entity/syncro.c
+++ src/runtime/stream/entity/syncro.c
@@ -185,22 +185,13 @@ static void SyncBoxTask(snet_entity_t *ent, void *arg)
             /* this is the first sync */
             if (!partial_sync) {
               partial_sync = true;
-
-#ifdef USE_USER_EVENT_LOGGING
-              /* Emit a monitoring message of first record entering syncro cell */
-              SNetThreadingEventSignal( ent,
-                  SNetMonInfoCreate( EV_MESSAGE_IN, MON_RECORD, rec)
-                  );
-#endif
-
-            } else {
-#ifdef USE_USER_EVENT_LOGGING
-              /* Emit a monitoring message of another accepted record entering syncro cell */
-              SNetThreadingEventSignal( ent,
-                  SNetMonInfoCreate( EV_MESSAGE_IN, MON_RECORD, rec)
-                  );
-#endif
             }
+#ifdef USE_USER_EVENT_LOGGING
+            /* Emit a monitoring message of another accepted record entering syncro cell */
+            SNetThreadingEventSignal( ent,
+            		SNetMonInfoCreate( EV_MESSAGE_IN, MON_RECORD, rec)
+            );
+#endif
           }
         }
 
