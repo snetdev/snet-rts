87b23c0b8c428e0b015a1adcbb9f79447e30533b
Bert Gijsbers <gijsbers@science.uva.nl>
2013-05-05 21:08:02 +0200

    Fix all memory leaks.

--- examples/cannons-algorithm/boxes/Multiplication.c
+++ examples/cannons-algorithm/boxes/Multiplication.c
@@ -7,38 +7,48 @@
 
 #include "Multiplication.h"
 
-void *Multiplication (void* hnd, c4snet_data_t *submatrix_A, c4snet_data_t *submatrix_B, c4snet_data_t *size_submatrix, c4snet_data_t *position)
+void *Multiplication (
+        void* hnd,
+        c4snet_data_t *submatrix_A,
+        c4snet_data_t *submatrix_B,
+        c4snet_data_t *size_submatrix,
+        c4snet_data_t *position)
 {
 
-	int *int_submatrix_A = C4SNetGetData(submatrix_A);
-	int *int_submatrix_B = C4SNetGetData(submatrix_B);
+        int *int_submatrix_A = C4SNetGetData(submatrix_A);
+        int *int_submatrix_B = C4SNetGetData(submatrix_B);
+        int  int_size_matrix = *(int *) C4SNetGetData(size_submatrix);
+        int *int_submatrix_result;
+        int  i, j;
+        void *vptr = NULL;
+        c4snet_data_t *submatrix_result = C4SNetAlloc(CTYPE_int,
+                                              int_size_matrix * int_size_matrix,
+                                              &vptr);
+        int_submatrix_result = (int *) vptr;
 
-	int int_size_matrix = *(int *) C4SNetGetData(size_submatrix);
-	int int_position = *(int *) C4SNetGetData(position);
+        /*
+         * Elements are moved from their initial positions to align them
+         * so that the correct submatrix are multiplied with one another.
+         * Note that submatrix on the diagonal don't actually require alignment.
+         * Alignment is done by shifting the -th row of positions left
+         * and the -th column of positions up.
+         */
+        for (i = 0; i < int_size_matrix; i++)
+        {
+                for (j = 0; j < int_size_matrix; j++)
+                {
+                        int_submatrix_result[(i*int_size_matrix) + j] =
+                             int_submatrix_A[(i*int_size_matrix) + j] *
+                             int_submatrix_B[(i*int_size_matrix) + j];
+                }
+        }
 
-	int *int_submatrix_result = malloc(sizeof(int) * int_size_matrix*int_size_matrix);
+        C4SNetOut(hnd, 1, submatrix_result, position);
 
-	int ele_A_shift;
-	int ele_B_shift;
-	int i,j;
+        C4SNetFree(submatrix_A);
+        C4SNetFree(submatrix_B);
+        C4SNetFree(size_submatrix);
 
-	/*
-	 * Elements are moved from their initial positions to align them so that the correct submatrix are multiplied
-	 * with one another. Note that submatrix on the diagonal don't actually require alignment. Alignment is done
-	 * by shifting the -th row of positions left and the -th column of positions up.
-	 */
-	for (i = 0; i < int_size_matrix; i++)
-	{
-		for (j = 0; j < int_size_matrix; j++)
-		{
-			int_submatrix_result[(i*int_size_matrix) + j] = int_submatrix_A[(i*int_size_matrix) + j] * int_submatrix_B[(i*int_size_matrix) + j];
-		}
-	}
-
-	c4snet_data_t *result;
-
-	result = C4SNetCreate(CTYPE_int,int_size_matrix*int_size_matrix, int_submatrix_result);
-	C4SNetOut(hnd,1, result, position);
-
-	return hnd;
+        return hnd;
 }
+
--- examples/cannons-algorithm/boxes/Split.c
+++ examples/cannons-algorithm/boxes/Split.c
@@ -9,127 +9,164 @@
 #include "Split.h"
 
 
-void *Split (void* hnd, c4snet_data_t *matrix_A, c4snet_data_t *matrix_B, c4snet_data_t *size_matrix, c4snet_data_t *division)
+void *Split (
+        void* hnd,
+        c4snet_data_t *matrix_A,
+        c4snet_data_t *matrix_B,
+        c4snet_data_t *size_matrix,
+        c4snet_data_t *division)
 {
-	 int *int_matrix_A  = C4SNetGetData(matrix_A);
-	 int *int_matrix_B  = C4SNetGetData(matrix_B);
+        int *int_matrix_A  = C4SNetGetData(matrix_A);
+        int *int_matrix_B  = C4SNetGetData(matrix_B);
+        int int_size_matrix = *(int *) C4SNetGetData(size_matrix);
+        int int_division = *(int *) C4SNetGetData(division);
+        int i, j;
+        int repetition, row, col;
 
-	 int int_size_matrix = *(int *) C4SNetGetData(size_matrix);
-	 int int_division = *(int *) C4SNetGetData(division);
+        /*if (int_division <= 0 || int_size_matrix % sqrt(int_division) != 0)
+        {
+                printf("THe number of division is not possible. ");
+                return hnd;
+        }*/
 
+        int submatrix_in_one_row = sqrt(int_division);
+        int size_submatrix = (int_size_matrix / sqrt(int_division));
+        int varrow = 0;
+        int varcol = 0;
 
-	 int *int_result = malloc(sizeof(int) * int_size_matrix * int_size_matrix);
+        int *int_copy_matrix_A = malloc(sizeof(int) * int_size_matrix*int_size_matrix);
+        int *int_copy_matrix_B = malloc(sizeof(int) * int_size_matrix*int_size_matrix);
+        int *int_submatrix_A = malloc(sizeof(int) * size_submatrix*size_submatrix);
+        int *int_submatrix_B = malloc(sizeof(int) * size_submatrix*size_submatrix);
 
-	 int **int_submatrix_collect_A = malloc(sizeof(int *) * int_division);
-	 int **int_submatrix_collect_B = malloc(sizeof(int *) * int_division);
+        int position;
 
-	 int i, j, k, z;
+        for ( repetition = 0; repetition < int_size_matrix; repetition++ )
+        {
+                row = 0;
+                col = 0;
 
-	 int repetition, row, col;
+                for (i = 0; i < int_size_matrix; i++)
+                {
+                        for (j = 0; j < int_size_matrix; j++)
+                        {
+                                if ( (j + row + repetition) < int_size_matrix )
+                                {
+                                        int_copy_matrix_A[(i*int_size_matrix) + j] =
+                                             int_matrix_A[ ( (i * int_size_matrix) +
+                                                           (j + row + repetition) )];
+                                }
+                                else
+                                {
+                                        int_copy_matrix_A[(i*int_size_matrix) + j] =
+                                             int_matrix_A[ ( (i * int_size_matrix) +
+                                                           ((j + row + repetition) %
+                                                                 int_size_matrix) )];
+                                }
 
-	 /*if (int_division <= 0 || int_size_matrix % sqrt(int_division) != 0)
-	 {
-		 printf("THe number of division is not possible. ");
-		 return hnd;
-	 }*/
+                                if ( ( (i*int_size_matrix) + (col * int_size_matrix) +
+                                     (repetition * int_size_matrix) ) <
+                                     ( int_size_matrix * int_size_matrix ) )
+                                {
+                                        int_copy_matrix_B[(i*int_size_matrix) + j] =
+                                         int_matrix_B[ j + ( (i + col + repetition ) *
+                                                                  int_size_matrix) ];
+                                }
+                                else
+                                {
+                                        int_copy_matrix_B[(i*int_size_matrix) + j] =
+                                         int_matrix_B[ j + (( (i + col + repetition ) *
+                                                  int_size_matrix) %
+                                                 (int_size_matrix * int_size_matrix)) ];
+                                }
 
-	 int submatrix_in_one_row = sqrt(int_division);
-	 int size_submatrix = (int_size_matrix / sqrt(int_division));
-	 int varrow = 0;
-	 int varcol = 0;
+                                col++;
+                        }
+                        row++;
+                }
+                varrow = 0;
+                varcol = 0;
 
-	 int *int_submatrix_A;
-	 int *int_submatrix_B;
-	 int *int_copy_matrix_A = malloc(sizeof(int) * int_size_matrix*int_size_matrix);
-	 int *int_copy_matrix_B = malloc(sizeof(int) * int_size_matrix*int_size_matrix);
+                for ( position = 0; position < int_division; position++ )
+                {
+                        for (i = 0; i < size_submatrix; i++)
+                        {
+                                for (j = 0; j < size_submatrix; j++)
+                                {
+                                        int_submatrix_A[(i*size_submatrix) + j] =
+                                         int_copy_matrix_A[(i + (varrow * size_submatrix)) *
+                                         int_size_matrix + (j + (varcol * size_submatrix))];
+                                        int_submatrix_B[(i*size_submatrix) + j] =
+                                         int_copy_matrix_B[(i + (varrow * size_submatrix))*
+                                         int_size_matrix + (j + (varcol * size_submatrix))];
+                                }
+                        }
 
-	 int_submatrix_A = malloc(sizeof(int) * size_submatrix*size_submatrix);
-	 int_submatrix_B = malloc(sizeof(int) * size_submatrix*size_submatrix);
+                        // right
+                        if ( (position + 1) % (int_size_matrix/size_submatrix) == 0 )
+                        {
+                                varcol = 0;
+                                varrow++;
+                        }
+                        else //down and begin
+                        {
+                                varcol++;
+                        }
 
-	 int position;
+                        // send data
+                        c4snet_data_t *c4_subA = C4SNetCreate (CTYPE_int,
+                                                   size_submatrix*size_submatrix,
+                                                   int_submatrix_A);
+                        c4snet_data_t *c4_subB = C4SNetCreate (CTYPE_int,
+                                                   size_submatrix*size_submatrix,
+                                                   int_submatrix_B);
+                        c4snet_data_t *c4_size = C4SNetCreate(CTYPE_int, 1,
+                                                   &size_submatrix);
+                        c4snet_data_t *c4_pos  = C4SNetCreate(CTYPE_int, 1,
+                                                   &position);
+                        C4SNetOut(hnd, 2, c4_subA, c4_subB, c4_size, c4_pos);
+                }
+        }
 
-	 for ( repetition = 0; repetition < int_size_matrix; repetition++ )
-	 {
-		 row = 0;
-		 col = 0;
+        free(int_copy_matrix_A);
+        free(int_copy_matrix_B);
+        free(int_submatrix_A);
+        free(int_submatrix_B);
 
-		 for (i = 0; i < int_size_matrix; i++)
-		 {
-			 for (j = 0; j < int_size_matrix; j++)
-			 {
-				 if ( (j + row + repetition) < int_size_matrix )
-				 {
-					 int_copy_matrix_A[(i*int_size_matrix) + j] = int_matrix_A[ ( (i * int_size_matrix) + (j + row + repetition) )];
-				 }
-				 else
-				 {
-					 int_copy_matrix_A[(i*int_size_matrix) + j] = int_matrix_A[ ( (i * int_size_matrix) + ((j + row + repetition) % int_size_matrix) )];
-				 }
+        // Second message
+        // int_size_matrix * int_size_matrix * submatrix_in_one_row =
+        //                        number of operations of submatrix
+        int total_ele = int_size_matrix * int_division;
+        int count = 0;
+        void *vptr = NULL;
+        c4snet_data_t *c4_result = C4SNetAlloc (CTYPE_int,
+                                                int_size_matrix*int_size_matrix,
+                                                &vptr);
+        int *int_result = (int *) vptr;
 
-				 if ( ( (i*int_size_matrix) + (col * int_size_matrix) + (repetition * int_size_matrix) ) < ( int_size_matrix * int_size_matrix ) )
-				 {
-					 int_copy_matrix_B[(i*int_size_matrix) + j] = int_matrix_B[ j + ( (i + col + repetition ) * int_size_matrix) ];
-				 }
-				 else
-				 {
-					 int_copy_matrix_B[(i*int_size_matrix) + j] = int_matrix_B[ j + (( (i + col + repetition ) * int_size_matrix) % (int_size_matrix * int_size_matrix)) ];
-				 }
+        for (i = 0; i < int_size_matrix; i++)
+        {
+                for (j = 0; j < int_size_matrix; j++)
+                {
+                        int_result[(i*int_size_matrix) + j] = 0;
+                }
+        }
 
-				 col++;
-			 }
-			 row++;
-		 }
-		 varrow = 0;
-		 varcol = 0;
+        c4snet_data_t *c4_total_ele   = C4SNetCreate(CTYPE_int, 1, &total_ele);
+        c4snet_data_t *c4_count       = C4SNetCreate(CTYPE_int, 1, &count);
+        c4snet_data_t *c4_submatrix_1 = C4SNetCreate(CTYPE_int, 1, &submatrix_in_one_row);
+        c4snet_data_t *c4_size_subm_1 = C4SNetCreate(CTYPE_int, 1, &size_submatrix);
+        C4SNetOut(hnd, 1,
+                  c4_total_ele,
+                  c4_count,
+                  c4_submatrix_1,
+                  size_matrix,
+                  c4_size_subm_1,
+                  c4_result);
 
-		 for ( position = 0; position < int_division; position++ )
-		 {
-			 for (i = 0; i < size_submatrix; i++)
-			 {
-				 for (j = 0; j < size_submatrix; j++)
-				 {
-					 int_submatrix_A[(i*size_submatrix) + j] = int_copy_matrix_A[(i + (varrow * size_submatrix))*int_size_matrix + (j + (varcol * size_submatrix))];
-					 int_submatrix_B[(i*size_submatrix) + j] = int_copy_matrix_B[(i + (varrow * size_submatrix))*int_size_matrix + (j + (varcol * size_submatrix))];
-				 }
-			 }
+        C4SNetFree(matrix_A);
+        C4SNetFree(matrix_B);
+        C4SNetFree(division);
 
-			 // right
-			 if ( (position + 1)% (int_size_matrix/size_submatrix) == 0 )
-			 {
-				 varcol = 0;
-				 varrow++;
-			 }
-			 else //down and begin
-			 {
-				 varcol++;
-			 }
-
-			 // send data
-			 C4SNetOut(hnd, 2, C4SNetCreate (CTYPE_int, size_submatrix*size_submatrix, int_submatrix_A),
-					 C4SNetCreate (CTYPE_int, size_submatrix*size_submatrix, int_submatrix_B),
-					 C4SNetCreate(CTYPE_int, 1, &size_submatrix), C4SNetCreate(CTYPE_int, 1, &position));
-		 }
-	 }
-
-	 // Second message
-	 // int_size_matrix * int_size_matrix * submatrix_in_one_row = number of operations of submatrix
-	 int total_ele = int_size_matrix * int_division;
-
-	 int count = 0;
-
-	 c4snet_data_t *result;
-	 for (i = 0; i < int_size_matrix; i++)
-	 {
-		 for (j = 0; j < int_size_matrix; j++)
-		 {
-			 int_result[(i*int_size_matrix) + j] = 0;
-		 }
-	 }
-
-	 result 	= C4SNetCreate (CTYPE_int, int_size_matrix*int_size_matrix, int_result);
-
-
-	 C4SNetOut(hnd, 1, C4SNetCreate(CTYPE_int, 1, &total_ele), C4SNetCreate(CTYPE_int, 1, &count), C4SNetCreate(CTYPE_int, 1, &submatrix_in_one_row), size_matrix, C4SNetCreate(CTYPE_int, 1, &size_submatrix), result);
-
-	return hnd;
+        return hnd;
 }
