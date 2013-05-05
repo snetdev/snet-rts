bcb9b7b6738e7bd02902db9012cebab3faf56cc0
Bert Gijsbers <gijsbers@science.uva.nl>
2013-05-05 11:37:28 +0200

    add 'crypto' application. add -t option. make make silent with -s.

--- examples/testperf.sh
+++ examples/testperf.sh
@@ -2,27 +2,31 @@
 #
 # Test various example applications and show timings.
 #
-# Usage: testperf.sh [ -c | -r ]
+# Usage: testperf.sh [ -c | -r | -t threading ]
 #
 
 # The applications to be tested
 APPLICATIONS="fibonacci poweroftwo sieve loadbalance"
 APPLICATIONS+=" cannons-algorithm matrix jpegCode"
-APPLICATIONS+=" cholesky"
+APPLICATIONS+=" cholesky crypto"
 
 # Which threading layers to test
 THREAD="pthread lpel lpel_hrc front"
 
-# Check options -c for compile and -r for run.
+# Check options: -c for compile, -r for run, -t for threading.
 com=
 run=
-for f
+thr=
+while [ $# -gt 0 ]
 do
-    case "$f" in
+    case "$1" in
         -c) com=1 ;;
         -r) run=1 ;;
-        *) echo "Usage: $0 [ -c | -r ]" ; exit 1 ;;
+        -t) shift ; thr+=" $1" ;;
+        *) echo "$0: Unknown option: $1"  >&2
+           echo "Usage: $0 [ -c | -r | -t threading ... ]" >&2; exit 1 ;;
     esac
+    shift
 done
 
 # If no options were given, then set them all.
@@ -30,6 +34,10 @@ if [ -z "$com" ] && [ -z "$run" ]
 then
     com=1
     run=1
+fi
+if [ -z "$thr" ]
+then
+    thr=$THREAD
 fi
 
 # Compile all executables for all threadings
@@ -41,8 +49,8 @@ then
         echo "######################################################################"
         echo "### make in $d "
         echo
-        make -C $d clean everything >/dev/null || exit 1
-        make -C $d all $THREAD >/dev/null || exit 1
+        make -s -C $d clean everything >/dev/null || exit 1
+        make -s -C $d all $thr >/dev/null || exit 1
     done
 fi
 
@@ -55,7 +63,7 @@ then
         echo "######################################################################"
         echo "### run in $d "
         echo
-        make -C $d test
+        make -s -C $d test
     done
 fi
 
